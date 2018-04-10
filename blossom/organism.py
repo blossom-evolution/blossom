53cd846ac6b268c0131c0b201b6deaf5488a6871
Bryan Brzycki <bbrzycki@college.harvard.edu>
2018-04-10 11:32:39 -0400

    Change syntax of make organism class

--- blossom/organism.py
+++ blossom/organism.py
@@ -1,16 +1,17 @@
 import organism_classes as oc
 
 # maybe put this in species.py? depends on use in executable
-def make_organism_class(reproduction_class,
-                        movement_class,
-                        food_class,
-                        water_class):
-    class Organism(reproduction_class,
-                   movement_class,
-                   food_class,
-                   water_class):
+def make_organism_class(movement,
+                        reproduction,
+                        drinking,
+                        eating):
+    class Organism(movement=movement,
+                   reproduction=reproduction,
+                   drinking=drinking,
+                   eating=eating):
         pass
     return Organism
 
 # initialization should do the randomization
-organism1 = Organism(r,m,f,w)
+MyOrganism = make_organism_class(m,r,d,e)
+organism1 = MyOrganism()

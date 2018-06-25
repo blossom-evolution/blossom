22571e08002167a4adb1e29c2b5d95fc6017491f
Bryan Brzycki <bbrzycki@berkeley.edu>
2018-06-24 21:11:21 -0700

    Update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='blossom',
-    version='0.1.2',
+    version='0.1.3',
     author='Bryan Brzycki',
     author_email='bbrzycki@berkeley.edu',
     description='A simple evolution simulator',

2e6bfb3eaba5b29f5d927514f268e7da737c1362
Bryan Brzycki <bbrzycki@berkeley.edu>
2018-08-16 03:13:39 -0700

    Made some additional datasets

--- scripts/parameter_load.py
+++ scripts/parameter_load.py
@@ -9,13 +9,14 @@ ORGANISM_FNS = None
 WORLD_PARAM_FN = 'world.env'
 SPECIES_PARAM_FNS = ['species1.org']
 START_TIME = 0
-END_TIME = 10
+END_TIME = 1024
 
 universe = blossom.Universe(world_fn=WORLD_FN,
                             organism_fns=ORGANISM_FNS,
                             world_param_fn=WORLD_PARAM_FN,
                             species_param_fns=SPECIES_PARAM_FNS,
                             current_time=START_TIME,
-                            end_time=END_TIME)
+                            end_time=END_TIME,
+                            dataset_dir='datasets/org1024_world1024_time1024/')
 while universe.current_time < universe.end_time:
     universe.step()
